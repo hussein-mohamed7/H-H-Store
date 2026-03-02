2537b629d2737e64397841fab32fe17cb1cf47f6
Haz1897 <haz25ayman@gmail.com>
2026-03-02 14:14:44 +0200

    update

---
+++ Backend/index.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = exprss();
+
+app.use(bodyParser.json());
+
+app.get("/");
+
+app.post("/")
+
