97aaa237597df8d0f50ca7b14f5e1e127ba89f22
Hussein2003 <husseinmohamed72003@gmail.com>
2026-03-06 17:41:02 +0200

    add home

--- Frontend/src/app/components/home-page/home-page.ts
+++ Frontend/src/app/components/home-page/home-page.ts
@@ -1,19 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductCategories } from "../product-categories/product-categories";
-<<<<<<< HEAD
 import { CardSlider } from "../card-slider/card-slider";
 
 @Component({
   selector: 'app-home-page',
   imports: [CommonModule, ProductCategories, CardSlider],
-=======
 import { CardSlider } from '../card-slider/card-slider';
 
 @Component({
   selector: 'app-home-page',
   imports: [CommonModule, ProductCategories,CardSlider ],
->>>>>>> 38532b4d4e94c1882bb6312263eb2f96922a06ad
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
